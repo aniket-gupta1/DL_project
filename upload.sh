761991855e650a90b49baa416a1f5a018041aa97
Aniket <aniket.tcdav@gmail.com>
2022-03-07 10:38:56 -0500

    new_test

--- upload.sh
+++ upload.sh
@@ -3,5 +3,5 @@
 git add --all
 commit_string=$1
 git commit --message $commit_string 
-echo "Committed the changes with $commit_string! "
+echo "Committed the changes with this commit string -> $commit_string! "
 git push
