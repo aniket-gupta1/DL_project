a5d9cef134ebb4a33135620c789e2c38aa12768b
Aniket <aniket.tcdav@gmail.com>
2022-03-07 10:45:06 -0500

    removed_comment

--- NR/main.py
+++ NR/main.py
@@ -10,7 +10,7 @@ from torch.utils.data import DataLoader
 from tensorboardX import SummaryWriter
 from loguru import logger
 
-# removed a comment and added this line
+
 from utils import tensor2float, save_scalars, DictAverageMeter, SaveScene, make_nograd_func
 from datasets import transforms, find_dataset_def
 from models import NeuralRecon
